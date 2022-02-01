768241b3d426d186dde14ea04c23def9cecd3989
awsdang <97907235+awsdang@users.noreply.github.com>
2022-02-01 19:38:50 +0300

    Update custom-text.js

--- assets/work/text/custom-text.js
+++ assets/work/text/custom-text.js
@@ -29,6 +29,6 @@ txtFile01.onreadystatechange = function () {
 	
 	
 }
-txtFile01.open("GET", './text.txt', true);
+txtFile01.open("GET", 'www.awsdang.com/assets/work/text/text.txt', true);
 txtFile01.send(null);
 
